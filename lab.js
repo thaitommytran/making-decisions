5b9ab24e747926a97475d4812dee81dd05410be6
Thai Tran <thai.tommy.tran@gmail.com>
2022-05-04 14:43:01 -0500

    intermediate problems

--- lab.js
+++ lab.js
@@ -132,11 +132,20 @@ while (score < passingScore) {
 */
 
 //CODE HERE
+let changeMyMind = true;
+
+if (changeMyMind === true) {
+  changeMyMind = !changeMyMind;
+} else {
+  changeMyMind = !changeMyMind;
+}
 
 ////////// PROBLEM 10 //////////
 // Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
 
 //CODE HERE
+changeMyMind = !changeMyMind;
+console.log(changeMyMind);
 
 ////////// ADVANCED PROBLEMS //////////
 
